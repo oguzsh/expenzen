de76867ba073a467ce507c278d434767926c18de
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-01 02:45:06 +0300

    add: frontend structure

---
+++ frontend/entrypoints/application.tsx
@@ -0,0 +1,13 @@
+import App from 'App';
+import React from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
