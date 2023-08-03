2651f21885c1912b93ce62f3c15187ac17ea93a2
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-04 01:08:32 +0300

    devise auth routes and react route redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
+  devise_for(:users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'register'
+    },
+  )
+
   post '/graphql', to: 'graphql#execute'
+
   root 'dashboard#index'
+  get '*path', to: "dashboard#index", constraints: ->(request) { (!request.xhr? && request.format.html?) || request.format == :json }
 end
