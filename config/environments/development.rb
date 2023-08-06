8cebec36b47fe36a95731fb81d28a38ddd4b567d
Oguzhan İnce <oguzhan824@gmail.com>
2023-08-06 19:33:09 +0300

    [FT-13] Forgot password page and functionality (#8)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,18 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    from: 'from@example.com',
+    user_name: Rails.application.credentials.mail[:username],
+    password: Rails.application.credentials.mail[:password],
+    address: 'smtp.mailtrap.io',
+    domain: 'smtp.mailtrap.io',
+    port: '2525',
+    authentication: :cram_md5
+  }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
