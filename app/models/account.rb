33f7631c65de3a91f7b9de4db7a211f7fe1e74ef
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-10 22:32:31 +0300

    add: account model

---
+++ app/models/account.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :bigint           not null, primary key
+#  balance    :integer          not null
+#  name       :string           not null
+#  type       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_accounts_on_user_id  (user_id)
+#
+class Account < ApplicationRecord
+  belongs_to :user
+end
