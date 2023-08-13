43c9279a0da4e377d2ae82c5b9e26ae73c9a6a54
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-14 00:18:01 +0300

    update: codegen config

--- codegen.ts
+++ codegen.ts
@@ -1,13 +1,20 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
+
 const config: CodegenConfig = {
   overwrite: true,
-  schema: '/schema.graphql',
-  documents: 'frontend/**/*.ts{x}',
+  schema: 'schema.graphql',
+  documents: 'frontend/**/*.{ts,tsx}',
   generates: {
-    'frontend/graphql/types.ts': {
-      preset: 'client',
-      plugins: [],
+    'frontend/graphql/types.ts': { plugins: ['typescript'] },
+    'frontend/': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        extension: '.generated.tsx',
+        baseTypesPath: 'types.ts',
+        folder: '__generated__',
+      },
+      plugins: ['typescript-operations', 'typescript-react-apollo'],
     },
   },
 };
