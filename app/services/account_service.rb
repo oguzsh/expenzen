d6caaeab8b698e7402c4287f423e76d47da6ba5f
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-27 02:52:08 +0300

    update: accounts service refactored with base service

--- app/services/account_service.rb
+++ app/services/account_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AccountService
+class AccountService < BaseService
   def self.accounts(user_id:)
     Account.where(user_id: user_id) # TODO: Add pagination
   end
@@ -8,22 +8,19 @@ class AccountService
   def self.create_account(name:, balance:, account_type:, user_id:)
     Account.find_or_create_by!(name: name, balance: balance, account_type: account_type, user_id: user_id)
   rescue ActiveRecord::RecordInvalid => error
-    raise GraphQL::ExecutionError, error.message
+    raise_error(message: error.message)
   end
 
   def self.update_account(attributes)
-    Account.find(attributes[:id]).tap do |account|
-      account.update!(attributes)
-    end
-  rescue ActiveRecord::RecordNotFound
-    raise GraphQL::ExecutionError, 'Account not found!'
+    account = find_record(model: Account, id: attributes[:id])
+    account.update!(attributes)
+    account
   rescue ActiveRecord::RecordInvalid => error
-    raise GraphQL::ExecutionError, error.message
+    raise_error(message: error.message)
   end
 
   def self.delete_account(account_id:)
-    Account.find(account_id).destroy
-  rescue ActiveRecord::RecordNotFound
-    raise GraphQL::ExecutionError, 'Account not found!'
+    account = find_record(model: Account, id: account_id)
+    account.destroy
   end
 end
