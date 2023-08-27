8d96cb932021547528934362467364b44e782d48
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-27 03:03:40 +0300

    add: account services spec and schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_234256) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_235913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
     t.string "account_type", null: false
-    t.integer "balance", null: false
+    t.float "balance", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
