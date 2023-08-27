d41b151fb1efbf5afdb021f2e59ba1d0c6dbc2fe
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-27 04:05:11 +0300

    add: transactions gql query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,8 +10,15 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :accounts, [Types::AccountType], 'All financial accounts', null: false
+
+    field :transactions, [Types::TransactionType], 'All transaction data', null: false
+
     def accounts
       ::AccountService.accounts(user_id: context[:current_user].id)
+    end
+
+    def transactions
+      ::TransactionService.transactions_by_user(user_id: context[:current_user].id)
     end
   end
 end
