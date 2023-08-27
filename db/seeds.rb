6f1556c03f01e8782097b0573610d2fc79c6a789
Oguzhan Ince <oguzhan824@gmail.com>
2023-08-27 03:49:36 +0300

    update: seeder with new models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,41 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+2.times do |n|
+  email = "example-#{n + 1}@gmail.org"
+  password = 'password'
+
+  User.create!({ email: email, password: password, password_confirmation: password })
+end
+
+# Create Categories
+12.times do
+  Category.create!(name: Faker::Commerce.unique.department)
+end
+
+# Create Accounts for Users
+User.all.each do |user|
+  3.times do
+    Account.create!(
+      user: user,
+      name: Faker::Bank.name,
+      balance: Faker::Commerce.price(range: 1000..10_000.0),
+      account_type: %w[savings checking].sample
+    )
+  end
+end
+
+# Create Transactions for Accounts
+Account.all.each do |account|
+  4.times do
+    Transaction.create!(
+      account: account,
+      user: account.user,
+      category: Category.all.sample,
+      transaction_date: Faker::Date.between(from: 30.days.ago, to: Time.zone.now),
+      amount: Faker::Commerce.price(range: 1.0..1000.0),
+      transaction_type: %w[income expense].sample
+    )
+  end
+end
+
+puts 'Seeding complete!'
