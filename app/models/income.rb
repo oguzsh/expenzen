8b155fc8d6fcde6a787a89b3d66538c549d4d374
Oguzhan Ince <oguzhan824@gmail.com>
2024-01-20 18:42:58 +0300

    add: income model and migration

---
+++ app/models/income.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: incomes
+#
+#  id               :bigint           not null, primary key
+#  is_recurring     :boolean          default(FALSE)
+#  recurring_period :integer          default("not_recurring")
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  account_id       :bigint           not null
+#
+# Indexes
+#
+#  index_incomes_on_account_id  (account_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#
+class Income < ApplicationRecord
+  include Recurring
+
+  belongs_to :account
+  has_many :transactions, as: transactable, dependent: :destory
+
+  validates :income_date, presence: true
+end
---
+++ db/migrate/20240120153137_create_incomes.rb
@@ -0,0 +1,11 @@
+class CreateIncomes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :incomes do |t|
+      t.references :account, null: false, foreign_key: true
+      t.boolean :is_recurring, default: false
+      t.integer :recurring_period, default: 0
+
+      t.timestamps
+    end
+  end
+end
