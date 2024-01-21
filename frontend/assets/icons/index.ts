99e939833ef167c9eb83e40694c0e9853cc090b8
Oguzhan Ince <oguzhan824@gmail.com>
2024-01-22 01:06:43 +0300

    add: new icons

--- frontend/assets/icons/index.ts
+++ frontend/assets/icons/index.ts
@@ -1,4 +1,6 @@
+import Dollar from './Dollar';
 import Notification from './Notification';
 import SignOut from './SignOut';
+import Wallet from './Wallet';
 
-export { Notification, SignOut };
+export { Notification, SignOut, Dollar, Wallet };
